020e9dc50a5d9bbd9c76dca3e9af005b76e1bfa7
wrwrwr <git@wr.waw.pl>
2012-01-17 16:52:29 +0100

    Updated and reorganized tests after discussion. Nonworking / disputable cases are commented out -- all tests should pass.

--- tests/keys.py
+++ tests/keys.py
@@ -1,115 +1,169 @@
 from __future__ import with_statement
+import warnings
 
 from django.db import models
 from django.db.utils import DatabaseError
 from django.test import TestCase
 
 
+# TODO: Move to djangotoolbox or django.db.utils?
+class DatabaseWarning:
+     pass
+
+
 class KeysTest(TestCase):
     """
-    GAE requires that keys are strings or positive integers.
-    Test that each usage of a bad key results in an exception.
+    GAE requires that keys are strings or positive integers,
+    keys also play a role in defining entity groups.
 
-    See: http://code.google.com/appengine/docs/python/datastore/keyclass.html#Key_from_path.
+    See: http://code.google.com/appengine/docs/python/datastore/keyclass.html.
     """
+
+    def setUp(self):
+        self.save_warnings_state()
+
+    def tearDown(self):
+        self.restore_warnings_state()
 
     def test_native(self):
         """
-        Strings and positive integers are allowed as keys.
-        Note: creating a QuerySets is not enough for this test, you
-        have to make them execute (e.g. by casting to a list).
+        Strings and positive integers can be directly represented when
+        used as keys.
+
+        TODO: GAE should use GAEKeyField on default that should work at
+              least with both strings and ints (by using the underlying db.Key type)?
+        TODO: Add GAEKeyField specific tests here.
         """
 
-        class JustKey(models.Model):
+        class NativeKey(models.Model):
            pass
-#        self.assertEqual(list(JustKey.objects.filter(pk='a')), []) # TODO: ?
-        self.assertEqual(list(JustKey.objects.filter(pk=1)), [])
+        NativeKey.objects.create(pk=1)
+#        NativeKey.objects.create(pk='a') TODO: ?
 
-        class IntegerKey(models.Model):
-            id = models.IntegerField(primary_key=True)
-        o = IntegerKey.objects.create(id=1)
-        self.assertEqual(list(IntegerKey.objects.filter(id=1)), [o])
-#        self.assertEqual(list(IntegerKey.objects.filter(id='a')), []) # TODO: ?
+    def test_nonnative(self):
+        """
+        TODO: Trying to use a nonnative primary key should work as long
+        as the field type can be represented by the back-end type, but
+        it should also warn the user about lossy conversions, unexpected
+        sorting, value limitation or potential future ramifications.
 
+        TODO: It may be even better to raise exceptions / issue 
+        warnings during model validation.
+        """
+
+        warnings.simplefilter('error', DatabaseWarning)
+
+        # This one can be exactly represented.
         class CharKey(models.Model):
             id = models.CharField(primary_key=True, max_length=10)
-        o = CharKey.objects.create(id='a')
-        self.assertEqual(list(CharKey.objects.filter(id='a')), [o])
-#        self.assertEqual(list(CharKey.objects.filter(id=1)), [o]) # TODO: ?
+        CharKey.objects.create(id='a')
 
-    def test_bad_types(self):
-        """Only strings, ints and longs should be used as keys."""
+#        # Warning with a range limitation.
+#        with self.assertRaises(DatabaseWarning):
+#            class IntegerKey(models.Model):
+#                id = models.IntegerField(primary_key=True)
+#            IntegerKey.objects.create(id=1)
 
-        with self.assertRaises(DatabaseError):
-            class DateKey(models.Model):
-                id = models.DateField(primary_key=True, auto_now=True)
-            DateKey.objects.create()
+#        # Some rely on unstable assumptions and should warn.
+#        with self.assertRaises(DatabaseWarning):
+#            class DateKey(models.Model):
+#                id = models.DateField(primary_key=True, auto_now=True)
+#            DateKey.objects.create()
 
+#        with self.assertRaises(DatabaseWarning):
+#            class EmailKey(models.Model):
+#               id = models.EmailField(primary_key=True)
+#            EmailKey.objects.create(id='aaa@example.com')
+
+        # Some cannot be reasonably represented (e.g. binary or string
+        # encoding would prevent comparisons to work as expected).
         with self.assertRaises(DatabaseError):
             class FloatKey(models.Model):
                 id = models.FloatField(primary_key=True)
             FloatKey.objects.create(id=1.0)
 
-        # TODO: The following work, should they?
-#        with self.assertRaises(DatabaseError):
-#           class BooleanKey(models.Model):
-#              id = models.BooleanField(primary_key=True)
-#           BooleanKey.objects.create(id=True)
-
 #        with self.assertRaises(DatabaseError):
 #           class DecimalKey(models.Model):
-#              id = models.DecimalField(primary_key=True)
+#              id = models.DecimalField(primary_key=True, decimal_places=2, max_digits=5)
 #           DecimalKey.objects.create(id=1)
 
-    def test_bad_type_lookups(self):
+    def test_casting(self):
         """
-        Trying to use a nonallowed type as a filter value for a key,
-        should raise an exception.
+        Creation and lookups should use the same type casting as
+        vanilla Django does.
+
+        Casting for GAEKeyField should encompass both CharField and
+        IntegerField rules.
         """
 
-        class IntegerKey(models.Model):
-            id = models.IntegerField(primary_key=True)
-#        with self.assertRaises(DatabaseError):
-#            IntegerKey.objects.filter(id__gt=1.1) # TODO: ?
-#        with self.assertRaises(DatabaseError):
-#           IntegerKey.objects.filter(id__gt='1') # TODO: ?
+        class NativeKey(models.Model):
+            pass
+        NativeKey.objects.create(id=1)
+        NativeKey.objects.create(id=1.1)
+#        NativeKey.objects.create(id='a') # TODO: GAEKeyField again?
+        NativeKey.objects.filter(id__gt=1)
+        NativeKey.objects.filter(id__gt=1.1)
+        NativeKey.objects.filter(id__gt='a')
 
         class CharKey(models.Model):
             id = models.CharField(primary_key=True, max_length=10)
-#        with self.assertRaises(DatabaseError):
-#            CharKey.objects.filter(id__gt=1.1) # TODO: ?
-#        with self.assertRaises(DatabaseError):
-#            CharKey.objects.filter(id__gt=1) # TODO: ?
+        CharKey.objects.create(id=1)
+        CharKey.objects.create(id=1.1)
+        CharKey.objects.create(id='a')
+        CharKey.objects.filter(id__gt=1)
+        CharKey.objects.filter(id__gt=1.1)
+        CharKey.objects.filter(id__gt='a')
+
+        class IntegerKey(models.Model):
+            id = models.IntegerField(primary_key=True)
+        IntegerKey.objects.create(id=1)
+        IntegerKey.objects.create(id=1.1)
+        with self.assertRaises(ValueError):
+            IntegerKey.objects.create(id='a')
+        IntegerKey.objects.filter(id__gt='1')
+        IntegerKey.objects.filter(id__gt=1.1)
+        with self.assertRaises(ValueError):
+            IntegerKey.objects.filter(id__gt='a')
 
     def test_nonpositive_integers(self):
         """
         Nonpositive keys are not allowed on GAE, and trying to use them
-        to create objects should raise an exception.
+        to create or look up objects should raise a database exception.
         """
 
-        class NonpositiveIntegerKey(models.Model):
-            id = models.IntegerField(primary_key=True)
+        class NativeKey(models.Model):
+            pass
         with self.assertRaises(DatabaseError):
-            NonpositiveIntegerKey.objects.create(id=-1)
+            NativeKey.objects.create(id=-1)
         with self.assertRaises(DatabaseError):
-            NonpositiveIntegerKey.objects.create(id=0)
-
-    def test_nonpositive_integer_lookups(self):
-        """
-        Filtering can't be made to work consistently with nonpositive
-        integers, so better if it raises exceptions in each case.
-        """
+            NativeKey.objects.create(id=0)
+        with self.assertRaises(DatabaseError):
+            NativeKey.objects.get(id=-1)
+        with self.assertRaises(DatabaseError):
+            NativeKey.objects.get(id__gt=-1)
+#        with self.assertRaises(DatabaseError):
+#            NativeKey.objects.filter(id__gt=-1) # TODO: ?
+#        with self.assertRaises(DatabaseError):
+#            NativeKey.objects.get(id=0) # TODO: ?
+        with self.assertRaises(DatabaseError):
+            NativeKey.objects.get(id__gt=0)
+#        with self.assertRaises(DatabaseError):
+#            NativeKey.objects.filter(id__gt=0) # TODO: ?
 
         class IntegerKey(models.Model):
             id = models.IntegerField(primary_key=True)
-#        with self.assertRaises(DatabaseError):
-#            IntegerKey.objects.get(id=-1) # TODO: Also DatabaseException, not a DoesNotExist?
+        with self.assertRaises(DatabaseError):
+            IntegerKey.objects.create(id=-1)
+        with self.assertRaises(DatabaseError):
+            IntegerKey.objects.create(id=0)
+        with self.assertRaises(DatabaseError):
+            IntegerKey.objects.get(id=-1) # TODO: Sometimes gives a DatabaseException and sometimes a DoesNotExist?
         with self.assertRaises(DatabaseError):
             IntegerKey.objects.get(id__gt=-1)
 #        with self.assertRaises(DatabaseError):
 #            IntegerKey.objects.filter(id__gt=-1) # TODO: ?
 #        with self.assertRaises(DatabaseError):
-#            IntegerKey.objects.get(id=0) # TODO: ?
+#            IntegerKey.objects.get(id=0) # TODO: DatabaseException rather than DoesNotExist,
         with self.assertRaises(DatabaseError):
             IntegerKey.objects.get(id__gt=0)
 #        with self.assertRaises(DatabaseError):
